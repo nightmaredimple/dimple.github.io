5faa524d679654038c9dc94489b28c89c897b0a1
nightmaredimple <huangpiao2985@163.com>
2020-03-08 18:18:06 +0800

    Site updated: 2020-03-08 18:18:04

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 06 2020 19:52:15 GMT+0800 (中国标准时间)
+// build time:Sun Mar 08 2020 18:18:01 GMT+0800 (中国标准时间)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
